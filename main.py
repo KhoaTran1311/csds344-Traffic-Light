7410889ebc852d06e6c10707efa94b31c26c74ff
Minh Khoa Tran <kmt141@case.edu>
2024-02-20 01:28:35 -0500

    polished pedestrian light

--- main.py
+++ main.py
@@ -1,85 +1,65 @@
-
 import pygame
-import sys
 import time
 
-# Constants for colors
-BLACK = (0, 0, 0)
+# colors
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 GREEN = (0, 255, 0)
 
-# Constants for time durations in seconds
+# time
 GREEN_TIME = 1.5
 YELLOW_TIME = 0.5
 RED_TIME = 2
-PED_GREEN_TIME = 0
 
-# Constants for simulation state
+# state
 VEHICLE_SECURE_STATES = ["NS_GREEN", "EW_GREEN"]
-PED_SECURE_STATES = ["WALK"]
 
-# Initialize Pygame
+# init Pygame
 pygame.init()
 
-# Set up the display
+# set up pygame
 WIDTH, HEIGHT = 400, 400
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Traffic Light Simulation")
 
-# Load the background image
-background_image = pygame.image.load("./images/img.png").convert()  # Add this line
-background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))  # Scale the image
-
+# images
+background_image = pygame.image.load("./images/img.png").convert()
+background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
 ped_red_light = pygame.image.load("./images/ped_red.png")
 ped_red_light = pygame.transform.scale(ped_red_light, (20,20))
 ped_green_light = pygame.image.load("./images/ped_green.png")
 ped_green_light = pygame.transform.scale(ped_green_light, (20,20))
 
-# Main simulation loop
+# main simulation
 def run_simulation():
     x_vehicle_state = "NS_GREEN"
     y_vehicle_state = "NS_RED"
-    ped_state = "WALK"
 
     while True:
-        # for event in pygame.event.get():
-        #     if event.type == pygame.QUIT:
-        #         pygame.quit()
-        #         sys.exit()
-
         screen.blit(background_image, (0,0))
 
         if x_vehicle_state == "NS_GREEN":
             draw_traffic_lights(GREEN, RED)
-            # draw_pedestrian_lights(GREEN)
             pygame.display.flip()
-            time.sleep(PED_GREEN_TIME)
-            # draw_pedestrian_lights(RED)
             draw_traffic_lights(RED, GREEN)
             pygame.display.flip()
-            time.sleep(GREEN_TIME - PED_GREEN_TIME)
+            time.sleep(GREEN_TIME)
             x_vehicle_state = "NS_YELLOW"
         elif x_vehicle_state == "NS_YELLOW":
             draw_traffic_lights(RED, YELLOW)
-            # draw_pedestrian_lights(RED)
             pygame.display.flip()
             time.sleep(YELLOW_TIME)
             x_vehicle_state = "NS_RED"
         elif x_vehicle_state == "NS_RED":
             if y_vehicle_state == "NS_RED":
                 draw_traffic_lights(RED, RED)
-                # draw_pedestrian_lights(GREEN)
                 pygame.display.flip()
-                time.sleep(PED_GREEN_TIME)
-                # draw_pedestrian_lights(RED)
                 draw_traffic_lights(GREEN, RED)
                 pygame.display.flip()
-                time.sleep(GREEN_TIME - PED_GREEN_TIME)
+                time.sleep(GREEN_TIME)
                 y_vehicle_state = "NS_YELLOW"
             elif y_vehicle_state == "NS_YELLOW":
                 draw_traffic_lights(YELLOW, RED)
-                # draw_pedestrian_lights(RED)
                 pygame.display.flip()
                 time.sleep(YELLOW_TIME)
                 y_vehicle_state = "NS_RED"
@@ -88,27 +68,15 @@ def run_simulation():
         pygame.display.flip()
 
 def draw_traffic_lights(ns_color, ew_color):
-    # light_width = 20
-    # light_height = 40
-    # radius = 10
-
     one_set_of_light_horizontal(ew_color, WIDTH // 5, HEIGHT // 4 * 3)
     one_set_of_light_horizontal(ew_color, WIDTH // 5 * 4, HEIGHT // 4)
     one_set_of_light(ns_color, WIDTH // 4, HEIGHT // 5)
     one_set_of_light(ns_color, WIDTH // 4 * 3, HEIGHT // 5 * 4)
 
-    # pygame.draw.rect(screen, ns_color,
-    #                  (WIDTH // 2 - light_width // 2, HEIGHT // 3 - light_height // 2, light_width, light_height))
-    # pygame.draw.rect(screen, ns_color,
-    #                  (WIDTH // 2 - light_width // 2, HEIGHT // 3 * 2 - light_height // 2, light_width, light_height))
-    # pygame.draw.rect(screen, ew_color,
-    #                  (WIDTH // 3 - light_height // 2, HEIGHT // 2 - light_width // 2, light_height, light_width))
-    #
-
 
 def one_set_of_light(color, x, y):
-    x_ped = abs(100 - x) // 1.4 + 120
-    y_ped = abs(100 - y) // 1.002 + 71
+    x_ped = abs(200 - x) + 90
+    y_ped = abs(100 - y) // 0.8 + 40
     pygame.draw.rect(screen, "gray17",
                       (x - 15, y-35, 30, 70))
     if color == (0, 255, 0):
@@ -143,8 +111,8 @@ def one_set_of_light(color, x, y):
         screen.blit(ped_green_light, (x_ped, y_ped))
 
 def one_set_of_light_horizontal(color, x, y):
-    x_ped = abs(10 - y)
-    y_ped = abs(100 - x) // 1.4 + 105
+    x_ped = abs(100 - y) // 0.75 + 60
+    y_ped = abs(200 - x) + 70
     pygame.draw.rect(screen, "gray17",
                       (x-35, y-15, 70, 30))
     if color == (0, 255, 0):
@@ -178,13 +146,6 @@ def one_set_of_light_horizontal(color, x, y):
                          (x_ped - 5, y_ped - 5, 30, 30))
         screen.blit(ped_green_light, (x_ped, y_ped))
 
-
-# def draw_pedestrian_lights(color):
-#     radius = 20
-#     pygame.draw.circle(screen, color, (WIDTH // 6, HEIGHT // 2), radius)
-#     pygame.draw.circle(screen, color, (WIDTH * 5 // 6, HEIGHT // 2), radius)
-#     pygame.draw.circle(screen, color, (WIDTH // 2, HEIGHT // 6), radius)
-#     pygame.draw.circle(screen, color, (WIDTH // 2, HEIGHT * 5 // 6), radius)
 
 if __name__ == "__main__":
     run_simulation()
