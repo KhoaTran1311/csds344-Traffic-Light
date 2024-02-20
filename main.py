b9f450157cd677cee697497df77558ea40652cde
Minh Khoa Tran <kmt141@case.edu>
2024-02-20 01:19:07 -0500

    final commit

--- main.py
+++ main.py
@@ -27,6 +27,15 @@ WIDTH, HEIGHT = 400, 400
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Traffic Light Simulation")
 
+# Load the background image
+background_image = pygame.image.load("./images/img.png").convert()  # Add this line
+background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))  # Scale the image
+
+ped_red_light = pygame.image.load("./images/ped_red.png")
+ped_red_light = pygame.transform.scale(ped_red_light, (20,20))
+ped_green_light = pygame.image.load("./images/ped_green.png")
+ped_green_light = pygame.transform.scale(ped_green_light, (20,20))
+
 # Main simulation loop
 def run_simulation():
     x_vehicle_state = "NS_GREEN"
@@ -38,6 +47,8 @@ def run_simulation():
         #     if event.type == pygame.QUIT:
         #         pygame.quit()
         #         sys.exit()
+
+        screen.blit(background_image, (0,0))
 
         if x_vehicle_state == "NS_GREEN":
             draw_traffic_lights(GREEN, RED)
@@ -81,10 +92,10 @@ def draw_traffic_lights(ns_color, ew_color):
     # light_height = 40
     # radius = 10
 
-    one_set_og_light(ew_color, WIDTH // 6, HEIGHT // 2)
-    one_set_og_light(ew_color, WIDTH // 6 * 5, HEIGHT // 2)
-    one_set_og_light(ns_color, WIDTH // 2, HEIGHT // 6)
-    one_set_og_light(ns_color, WIDTH // 2, HEIGHT // 6 * 5)
+    one_set_of_light_horizontal(ew_color, WIDTH // 5, HEIGHT // 4 * 3)
+    one_set_of_light_horizontal(ew_color, WIDTH // 5 * 4, HEIGHT // 4)
+    one_set_of_light(ns_color, WIDTH // 4, HEIGHT // 5)
+    one_set_of_light(ns_color, WIDTH // 4 * 3, HEIGHT // 5 * 4)
 
     # pygame.draw.rect(screen, ns_color,
     #                  (WIDTH // 2 - light_width // 2, HEIGHT // 3 - light_height // 2, light_width, light_height))
@@ -95,7 +106,9 @@ def draw_traffic_lights(ns_color, ew_color):
     #
 
 
-def one_set_og_light(color, x, y):
+def one_set_of_light(color, x, y):
+    x_ped = abs(100 - x) // 1.4 + 120
+    y_ped = abs(100 - y) // 1.002 + 71
     pygame.draw.rect(screen, "gray17",
                       (x - 15, y-35, 30, 70))
     if color == (0, 255, 0):
@@ -105,6 +118,9 @@ def one_set_og_light(color, x, y):
                            (x, y), 10)
         pygame.draw.circle(screen, (128, 128, 128),
                            (x, y - 20), 10)
+        pygame.draw.rect(screen, "gray17",
+                         (x_ped - 5, y_ped - 5, 30, 30))
+        screen.blit(ped_red_light, (x_ped, y_ped))
     elif color == (255, 255, 0):
         pygame.draw.circle(screen, (128, 128, 128),
                        (x, y + 20), 10)
@@ -112,6 +128,9 @@ def one_set_og_light(color, x, y):
                            (x, y), 10)
         pygame.draw.circle(screen, (128, 128, 128),
                            (x, y - 20), 10)
+        pygame.draw.rect(screen, "gray17",
+                         (x_ped - 5, y_ped - 5, 30, 30))
+        screen.blit(ped_red_light, (x_ped, y_ped))
     elif color == (255, 0, 0):
         pygame.draw.circle(screen, (128, 128, 128),
                            (x, y + 20), 10)
@@ -119,6 +138,45 @@ def one_set_og_light(color, x, y):
                            (x, y), 10)
         pygame.draw.circle(screen, color,
                            (x, y - 20), 10)
+        pygame.draw.rect(screen, "gray17",
+                         (x_ped-5, y_ped-5, 30, 30))
+        screen.blit(ped_green_light, (x_ped, y_ped))
+
+def one_set_of_light_horizontal(color, x, y):
+    x_ped = abs(10 - y)
+    y_ped = abs(100 - x) // 1.4 + 105
+    pygame.draw.rect(screen, "gray17",
+                      (x-35, y-15, 70, 30))
+    if color == (0, 255, 0):
+        pygame.draw.circle(screen, color,
+                       (x + 20, y), 10)
+        pygame.draw.circle(screen, (128, 128, 128),
+                           (x, y), 10)
+        pygame.draw.circle(screen, (128, 128, 128),
+                           (x - 20, y), 10)
+        pygame.draw.rect(screen, "gray17",
+                         (x_ped - 5, y_ped - 5, 30, 30))
+        screen.blit(ped_red_light, (x_ped, y_ped))
+    elif color == (255, 255, 0):
+        pygame.draw.circle(screen, (128, 128, 128),
+                       (x + 20, y), 10)
+        pygame.draw.circle(screen, color,
+                           (x, y), 10)
+        pygame.draw.circle(screen, (128, 128, 128),
+                           (x - 20, y), 10)
+        pygame.draw.rect(screen, "gray17",
+                         (x_ped - 5, y_ped - 5, 30, 30))
+        screen.blit(ped_red_light, (x_ped, y_ped))
+    elif color == (255, 0, 0):
+        pygame.draw.circle(screen, (128, 128, 128),
+                           (x + 20, y), 10)
+        pygame.draw.circle(screen, (128, 128, 128),
+                           (x, y), 10)
+        pygame.draw.circle(screen, color,
+                           (x - 20, y), 10)
+        pygame.draw.rect(screen, "gray17",
+                         (x_ped - 5, y_ped - 5, 30, 30))
+        screen.blit(ped_green_light, (x_ped, y_ped))
 
 
 # def draw_pedestrian_lights(color):
